aa5d7152fc70ad3de823f375354d9c16950ce2dc
Attila Zsolt Danku <attila@apexlab.io>
2023-11-07 16:08:28 +0200

    fix: birthday confirm

--- packages/functions/events/birthdayFilled.ts
+++ packages/functions/events/birthdayFilled.ts
@@ -4,12 +4,16 @@ import { handleEvent } from "@/utils/eventBridge/handleEvent";
 export const handler = handleEvent(
   "birthdayFilled",
   async ({ birthday, responseUrl }) => {
-    await fetch(responseUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(constructConfirmBirthdayMessage(birthday)),
-    });
+    try {
+      await fetch(responseUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(constructConfirmBirthdayMessage(birthday)),
+      });
+    } catch (error) {
+      console.error("birthdayFilled", error);
+    }
   },
 );
