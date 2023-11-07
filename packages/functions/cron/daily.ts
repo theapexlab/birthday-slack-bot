ec80335f90551cab790f90a55170d9863c127774
Balint Dolla <balint@apexlab.io>
2023-11-07 21:51:17 +0100

    chore: add missing try catch blocks and error logs

--- packages/functions/cron/daily.ts
+++ packages/functions/cron/daily.ts
@@ -8,22 +8,26 @@ import { publishEvent } from "@/utils/eventBridge/publishEvent";
 import { cronHandler } from "@/utils/lambda/cronHandler";
 
 export const handler = cronHandler(async (eventId?: string) => {
-  const today = dayjs().utc().startOf("day");
+  try {
+    const today = dayjs().utc().startOf("day");
 
-  const users = await getBirthdays(today.add(2, "month"));
+    const users = await getBirthdays(today.add(2, "month"));
 
-  await Promise.all(
-    users.map((user) =>
-      publishEvent("askPresentIdeasFromTeam", {
-        team: user.teamId,
-        birthdayPerson: user.id,
-        eventId,
-      }),
-    ),
-  );
+    await Promise.all(
+      users.map((user) =>
+        publishEvent("askPresentIdeasFromTeam", {
+          team: user.teamId,
+          birthdayPerson: user.id,
+          eventId,
+        }),
+      ),
+    );
 
-  return {
-    users,
-    message: users.length ? "Sent present ideas requests" : "No birthdays",
-  };
+    return {
+      users,
+      message: users.length ? "Sent present ideas requests" : "No birthdays",
+    };
+  } catch (error) {
+    console.error("Error processing daily cron: ", error);
+  }
 });
--- packages/functions/events/savePresentIdea.ts
+++ packages/functions/events/savePresentIdea.ts
@@ -5,19 +5,23 @@ import { handleEvent } from "@/utils/eventBridge/handleEvent";
 export const handler = handleEvent(
   "savePresentIdea",
   async ({ user, team, birthdayPerson, presentIdea, responseUrl }) => {
-    await savePresentIdea({
-      userId: user,
-      teamId: team,
-      birthdayPerson,
-      presentIdea,
-    });
+    try {
+      await savePresentIdea({
+        userId: user,
+        teamId: team,
+        birthdayPerson,
+        presentIdea,
+      });
 
-    await fetch(responseUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(constructPresentIdeaSavedMessage()),
-    });
+      await fetch(responseUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(constructPresentIdeaSavedMessage()),
+      });
+    } catch (error) {
+      console.error("Error processing savePresentIdea event: ", error);
+    }
   },
 );
