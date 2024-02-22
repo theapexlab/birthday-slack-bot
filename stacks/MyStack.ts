798d7ebb183fa3e4580f0fb5556453a47cc11144
David Debreceni <debrecenidavid0311@gmail.com>
2024-02-22 15:00:29 +0100

    feat: comment api out

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,53 +1,49 @@
 import type { StackContext } from "sst/constructs";
-import { Api, Function, Queue, use } from "sst/constructs";
+import { Function } from "sst/constructs";
 
-import { eventTypes } from "@/events";
-
-import { addTestRoutes } from "./utils/addTestRoutes";
-import { getDbFunctionProps, getFunctionProps } from "./utils/getFunctionProps";
-import { EventBusStack } from "./EventBusStack";
+import { getDbFunctionProps } from "./utils/getFunctionProps";
 
 export function MyStack({ stack }: StackContext) {
-  const { eventBus } = use(EventBusStack);
-  const functionProps = getFunctionProps();
+  // const { eventBus } = use(EventBusStack);
+  // const functionProps = getFunctionProps();
 
-  eventBus.addRules(
-    stack,
-    eventTypes.reduce(
-      (rules, eventType) => ({
-        ...rules,
-        [eventType]: {
-          pattern: { detailType: [eventType] },
-          targets: {
-            function: new Queue(stack, `${eventType}-queue`, {
-              consumer: {
-                function: {
-                  handler: `packages/functions/events/${eventType}.handler`,
-                  ...functionProps,
-                },
-              },
-            }),
-          },
-        },
-      }),
-      {},
-    ),
-  );
+  // eventBus.addRules(
+  //   stack,
+  //   eventTypes.reduce(
+  //     (rules, eventType) => ({
+  //       ...rules,
+  //       [eventType]: {
+  //         pattern: { detailType: [eventType] },
+  //         targets: {
+  //           function: new Queue(stack, `${eventType}-queue`, {
+  //             consumer: {
+  //               function: {
+  //                 handler: `packages/functions/events/${eventType}.handler`,
+  //                 ...functionProps,
+  //               },
+  //             },
+  //           }),
+  //         },
+  //       },
+  //     }),
+  //     {},
+  //   ),
+  // );
 
-  const api = new Api(stack, "Api", {
-    defaults: {
-      function: {
-        ...functionProps,
-      },
-    },
-    routes: {
-      "POST /slack/event": "packages/functions/lambdas/slack-event.handler",
-      "POST /slack/interaction":
-        "packages/functions/lambdas/slack-interaction.handler",
-    },
-  });
+  // const api = new Api(stack, "Api", {
+  //   defaults: {
+  //     function: {
+  //       ...functionProps,
+  //     },
+  //   },
+  //   routes: {
+  //     "POST /slack/event": "packages/functions/lambdas/slack-event.handler",
+  //     "POST /slack/interaction":
+  //       "packages/functions/lambdas/slack-interaction.handler",
+  //   },
+  // });
 
-  addTestRoutes(stack, api);
+  // addTestRoutes(stack, api);
 
   const migrationFn = new Function(stack, "MigrateDb", {
     handler: "packages/functions/lambdas/migrateDb.handler",
@@ -61,7 +57,7 @@ export function MyStack({ stack }: StackContext) {
   });
 
   stack.addOutputs({
-    ApiEndpoint: api.url,
+    // ApiEndpoint: api.url,
     MigrationFunctionName: migrationFn.functionName,
   });
 }
