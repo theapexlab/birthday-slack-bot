3fbdc1b3b62b90c3d94268689711f65123b153c4
David Debreceni <debrecenidavid0311@gmail.com>
2024-02-26 15:37:46 +0100

    feat: cleanup to generic sql lambda change back for syntax fix

--- packages/core/db/queries/cleanUp.ts
+++ packages/core/db/queries/cleanUp.ts
@@ -3,4 +3,6 @@ import { sql } from "drizzle-orm";
 import { db } from "@/db/index";
 
 export const cleanUp = async (executeSql: string) =>
-  db.execute(sql`${executeSql}`);
+  db.execute(
+    sql`SELECT table_name FROM information_schema.tables WHERE table_schema='public'`,
+  );
