c3c7a3eeb2fd73df8de358ea391d200fd55934d2
David Debreceni <debrecenidavid0311@gmail.com>
2024-02-26 15:00:51 +0100

    feat: add cleanup function name to test:ci env

--- tests/utils/unit/cleanUp.ts
+++ tests/utils/unit/cleanUp.ts
@@ -3,10 +3,10 @@ import { InvokeCommand, LambdaClient } from "@aws-sdk/client-lambda";
 const client = new LambdaClient({ region: "eu-central-1" });
 
 export const cleanUp = async (tableName: string) => {
-  console.log("🚀 ~ cleanUp ~ argv:", process.env.CLEANUP_FUNCTION_NAME);
+  console.log("🚀 ~ cleanUp ~ argv:", process.env.VITE_CLEANUP_FUNCTION_NAME);
   const res = await client.send(
     new InvokeCommand({
-      FunctionName: process.env.CLEANUP_FUNCTION_NAME,
+      FunctionName: process.env.VITE_CLEANUP_FUNCTION_NAME,
       Payload: JSON.stringify({ tableName }),
     }),
   );
