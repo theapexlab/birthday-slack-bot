f5622db1dc24152691d60a7fee7641f86698234c
David Debreceni <debrecenidavid0311@gmail.com>
2024-02-26 16:07:56 +0100

    feat: cleanup util timeout, result log

--- tests/utils/unit/cleanUp.ts
+++ tests/utils/unit/cleanUp.ts
@@ -1,26 +1,28 @@
-import { InvokeCommand, LambdaClient } from "@aws-sdk/client-lambda";
+// import { InvokeCommand, LambdaClient } from "@aws-sdk/client-lambda";
 
-const client = new LambdaClient({ region: "eu-central-1" });
+// const client = new LambdaClient({ region: "eu-central-1" });
 
-export const cleanUp = async (tableName: string) => {
-  console.log(
-    "🚀 ~ cleanUp ~ argv:",
-    process.env.VITE_CLEANUP_FUNCTION_NAME,
-    tableName,
-  );
-  const res = await client.send(
-    new InvokeCommand({
-      FunctionName: process.env.VITE_CLEANUP_FUNCTION_NAME,
-      Payload: JSON.stringify({
-        sql: `SELECT table_name FROM information_schema.tables WHERE table_schema='public'`,
-      }),
-    }),
-  );
-  console.log("🚀 ~ cleanUp ~ res:", res);
+export const cleanUp = async (tableName: string) =>
+  // {
+  tableName;
+//   console.log(
+//     "🚀 ~ cleanUp ~ argv:",
+//     process.env.VITE_CLEANUP_FUNCTION_NAME,
+//     tableName,
+//   );
+//   const res = await client.send(
+//     new InvokeCommand({
+//       FunctionName: process.env.VITE_CLEANUP_FUNCTION_NAME,
+//       Payload: JSON.stringify({
+//         sql: `SELECT table_name FROM information_schema.tables WHERE table_schema='public'`,
+//       }),
+//     }),
+//   );
+//   console.log("🚀 ~ cleanUp ~ res:", res);
 
-  //   const payload = res.Payload
-  //     ? Buffer.from(res.Payload?.buffer).toString()
-  //     : undefined;
-  return;
-  //   return JSON.parse(payload || "");
-};
+//   const payload = res.Payload
+//     ? Buffer.from(res.Payload?.buffer).toString()
+//     : undefined;
+//   return;
+//   return JSON.parse(payload || "");
+// };
